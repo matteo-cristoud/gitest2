f0db3997bcfe93f0c4ee8242195146e4c43a9e78
BOGLIOLI <admin@SN2-PC02.lgm.local>
2021-03-03 10:14:19 +0100

    ajout fichier python

---
+++ testrt.py
@@ -0,0 +1,17 @@
+# coding: UTF-8
+
+"""
+Script: pythonProject1/testrt
+Création: admin, le 03/03/2021
+"""
+
+
+# Imports
+# Fonctions
+# Programme principal
+def main():
+
+    print("Bonjour")
+if __name__ == '__main__':
+    main()
+# Fin
