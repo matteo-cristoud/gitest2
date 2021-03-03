4f736cf3200538c5b5d402ebb93ee00614ec860d
Evan Lacombe <lacombe.evan.lgm@gmail.com>
2021-03-03 10:15:11 +0100

    ajoutfichier python

---
+++ bonjour.py
@@ -0,0 +1,17 @@
+# coding: UTF-8
+"""
+Script: temperatureVilles/bonjour.py
+Création: elacombe, le 03/03/2021
+"""
+
+
+# Imports
+# Fonctions
+# Programme principal
+def main():
+    print("aurevoir")
+
+
+if __name__ == '__main__':
+    main()
+# Fin
